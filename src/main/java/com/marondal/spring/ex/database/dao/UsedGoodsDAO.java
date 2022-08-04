a14adcf0f14089f1db35f2bea54720d9ae4fd4ab
asdf <asdf@gmail.com>
2022-08-04 16:43:47 +0900

    [김인규]  - 데이터 베이스 연동 예제 1

---
+++ src/main/java/com/marondal/spring/ex/database/dao/UsedGoodsDAO.java
@@ -0,0 +1,15 @@
+package com.marondal.spring.ex.database.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.marondal.spring.ex.database.model.UsedGoods;
+
+@Repository
+public interface UsedGoodsDAO {
+	
+	// used goods 의 모든 행을 조회하는 기능
+	public List<UsedGoods> selectUsedGoodsList();
+
+}
